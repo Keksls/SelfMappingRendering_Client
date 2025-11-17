e9426f6fe6ecef44b16eb7de57030c78c890b4d9
Kekstop <k@k.k>
2025-11-17 15:42:33 +0100

    update build

--- Build/SMR.loader.js
+++ Build/SMR.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 155934596,
+    wasmFileSize: 155934533,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
