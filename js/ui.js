145244c1b98c064f09848a49ae6770e766d7d1a4
Kekstop <k@k.k>
2025-11-17 15:22:07 +0100

    some UI fixes

--- js/ui.js
+++ js/ui.js
@@ -472,7 +472,7 @@
             reset(placeholder) {
                 optionsBox.innerHTML = "";
                 display.textContent = placeholder;
-                this.value = "";
+                this._value = "";
             }
         };
 
