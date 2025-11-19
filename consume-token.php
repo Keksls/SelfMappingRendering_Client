ca1561eb25767f6e52c088e877cfce2e3e8a0326
Kekstop <k@k.k>
2025-11-19 17:38:31 +0100

    fix screenshot and add watermark

--- consume-token.php
+++ consume-token.php
@@ -1,36 +1,32 @@
 <?php
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once('/opt/bitnami/wordpress/wp-load.php');
-
+require_once('/opt/bitnami/wordpress/wp-blog-header.php');
 header('Content-Type: application/json');
 
-// Must be logged in
+// User not logged
 if (!is_user_logged_in()) {
-    echo json_encode([
-        "success" => false,
-        "redirect" => "/"
-    ]);
+    echo json_encode(["success" => false, "redirect" => "/"]);
     exit;
 }
 
 $user_id = get_current_user_id();
-$acf = function_exists('get_fields') ? get_fields("user_$user_id") : [];
 
+// load ACF fields
+$acf = get_fields("user_$user_id");
 $tokens = isset($acf['tokens']) ? intval($acf['tokens']) : 0;
 
 if ($tokens <= 0) {
-    echo json_encode([
-        "success" => false,
-        "redirect" => "/pricing/"
-    ]);
+    echo json_encode(["success" => false, "redirect" => "/pricing/"]);
     exit;
 }
 
-// Decrease token
-update_field("tokens", $tokens - 1, "user_$user_id");
+// IMPORTANT : mettre ici LE FIELD_KEY ACF
+$field_key = "tokens";
+
+// decrease token
+update_field($field_key, $tokens - 1, "user_$user_id");
 
 echo json_encode([
     "success" => true,
